60a15ec1331918465df98cc991be6a8d74819c79
Bhim Raj Bhandari <infobrb04@gmail.com>
2025-08-14 19:23:44 +0545

    Third Program

---
+++ src/main/java/com/kliv/spring/sprinffrof_iii/SprinfFrofIiiApplication.java
@@ -0,0 +1,13 @@
+package com.kliv.spring.sprinffrof_iii;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprinfFrofIiiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SprinfFrofIiiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kliv/spring/sprinffrof_iii/SprinfFrofIiiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kliv.spring.sprinffrof_iii;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprinfFrofIiiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
